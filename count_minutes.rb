e661249b12385f903570bb0ce7bcb447004317db
Tyler Buchanan <tmailbuchanan@yahoo.com>
2014-06-15 16:49:28 -0700

    solved count_minutes

--- count_minutes.rb
+++ count_minutes.rb
@@ -6,14 +6,68 @@
 #  For example: if str is 9:00am-10:00am then the output should be 60. 
 #  If str is 1:00pm-11:00am the output should be 1320.
 
-str = "9:00am-10:00pm"
+str = "9:00am-10:00am"
+str = "1:00pm-11:00am"
 def CountingMinutes(str)
-	int_one = str.to_i
-	if int_one < 10 && str[1] == ":" && 
-	# array = str.split("-")
-	# if array[0] < "10"
-	# 	p array[0]
+array = str.split("-")
+# function: if both time are am or both times are pm
+	if array[0].include?("am") && array[1].include?("am") || array[0].include?("pm") && array[1].include?("pm")
+		if str[0].to_i < 10 && str[1] == ":"
+			firstHour = str[0].to_i
+		elsif str[0].to_i < 10 && str[1] != ":"
+			firstHour = (str[0] + str[1]).to_i
+		else
+			firstHour = nil
+		end
+		
+		if str[7].to_i < 10 && str[8] == ":"
+			secondHour = str[7].to_i
+		elsif str[7].to_i < 10 && str[8] != ":"
+			secondHour = (str[7] + str[8]).to_i
+		else
+			nil
+		end
+	minutes = ((firstHour - secondHour).abs * 60)
+
+# function: if first time is am and second time is pm
+	elsif array[0].include?("am") && array[1].include?("pm")
+		if str[0].to_i < 10 && str[1] == ":"
+			firstHour = str[0].to_i
+		elsif str[0].to_i < 10 && str[1] != ":"
+			firstHour = (str[0] + str[1]).to_i
+		else
+			firstHour = nil
+		end
+		
+		if str[7].to_i < 10 && str[8] == ":"
+			secondHour = str[7].to_i
+		elsif str[7].to_i < 10 && str[8] != ":"
+			secondHour = (str[7] + str[8]).to_i
+		else
+			nil
+		end
+	minutes = (((firstHour - secondHour).abs - 12) * 60).abs
+
+# function: if second time is am and first time is pm
+	elsif array[0].include?("pm") && array[1].include?("am")
+		if str[0].to_i < 10 && str[1] == ":"
+			firstHour = str[0].to_i
+		elsif str[0].to_i < 10 && str[1] != ":"
+			firstHour = (str[0] + str[1]).to_i
+		else
+			firstHour = nil
+		end
+		
+		if str[7].to_i < 10 && str[8] == ":"
+			secondHour = str[7].to_i
+		elsif str[7].to_i < 10 && str[8] != ":"
+			secondHour = (str[7] + str[8]).to_i
+		else
+			nil
+		end
+	minutes = ((firstHour - (secondHour + 12)).abs * 60).abs
 	end
-	
 end
-CountingMinutes(str)
+p CountingMinutes(str)
+
+
